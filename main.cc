bef657690ca42fde3bf535551aa5c59ef0b22c36
Alaap Surendran <alaapsurendran@gmail.com>
2020-07-25 22:46:16 +0530

    Added basic memory structure

--- main.cc
+++ main.cc
@@ -1,7 +1,32 @@
 #include <iostream>
 
+class chip8 {
+private:
+    const short display_width = 64;
+    const short display_hight = 32;
+
+    union {
+        unsigned char Memory[0x1000];
+        struct {
+            unsigned char V[16];
+            unsigned short I;
+            unsigned char ST;
+            unsigned char DT;
+            unsigned short PC;
+            unsigned short SP;
+            unsigned short stack[16];
+            unsigned char display[display_width][display_hight];
+            unsigned char font[16][5];
+        };
+    };
+public:
+    chip8() {
+        
+    }
+};
+
+
 int main() {
-    std::cout << "Hello world" << std::endl;
     return 0;
 }
 
